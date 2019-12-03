d47540d55ddec5ffa61a962bf754868f9e9826d9
pjupju <tkachenkoyuriy12@gmail.com>
2019-12-03 11:24:00 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+document.querySelector('.menu-icon-wrapper').onclick = function(){
+    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+}
+$(function(){
+    $(".portfolio__content-slider-gallery").slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: true,
+        dots: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            }
+          ]
+    });
+  });
